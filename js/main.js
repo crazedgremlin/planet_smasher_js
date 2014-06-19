63b7620b6130987254fe289fc4609fc457f75236
Dan McArdle <zingermc@gmail.com>
2014-06-19 12:46:40 -0400

    Mouse controls

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ var MIN_COORD = -1000;
 var MAX_COORD = 1000;
 var TIME_COEFF = 0.005;
 
-var camera, scene, renderer;
+var camera, scene, renderer, controls;
 var geometry, material, mesh;
 
 var time = 0;
@@ -20,55 +20,80 @@ function init() {
 
     scene = new THREE.Scene();
 
-    geometry = new THREE.BoxGeometry(50,50,50);
-    material = new THREE.MeshBasicMaterial({
-        color: 0xff0000,
-        wireframe: true 
-    });
-
-    mesh = new THREE.Mesh(geometry, material);
-    scene.add(mesh);
-
-
-    var particleCount = 1800;
-    var particles = new THREE.Geometry();
-    var pMaterial = new THREE.ParticleBasicMaterial({
-          color: 0xFFFF99,
-          size: 4 
-    });
-   
-    for (var p=0; p<particleCount; p++) {
-
-        var extra = 1.5;
-        var pX = randVal(extra*MIN_COORD, extra*MAX_COORD);
-        var pY = randVal(extra*MIN_COORD, extra*MAX_COORD);
-        var pZ = randVal(extra*MIN_COORD, extra*MAX_COORD);
-        var particle = new THREE.Vector3(pX, pY, pZ);
-
-        particles.vertices.push(particle);
-    }
-
-    var particleSystem = new THREE.ParticleSystem(particles, pMaterial);
-    scene.add(particleSystem);
-
-    for (var i=0; i<planetArr.length; i++) {
-        var thisPlanet = planetArr[i];
-        
-        var planetMaterial = new THREE.MeshBasicMaterial({
-            color: 0x00ddff,
+    (function createRedBox() {
+        geometry = new THREE.BoxGeometry(50,50,50);
+        material = new THREE.MeshBasicMaterial({
+            color: 0xff0000,
             wireframe: true 
         });
-        var planetGeometry = new THREE.BoxGeometry(100, 100, 100);
-        planetGeometry.applyMatrix(
-            new THREE.Matrix4().makeTranslation(thisPlanet.x, thisPlanet.y, thisPlanet.z)
-        );
-        var planetMesh = new THREE.Mesh(planetGeometry, planetMaterial);
+        mesh = new THREE.Mesh(geometry, material);
+        scene.add(mesh);
+    })();
 
-        scene.add(planetMesh);
-    }
+    (function createStarfield() {
+        var particleCount = 1800;
+        var particles = new THREE.Geometry();
+        var pMaterial = new THREE.ParticleBasicMaterial({
+              color: 0xFFFF99,
+              size: 4 
+        });
+
+        for (var p=0; p<particleCount; p++) {
+
+            var extra = 1.5;
+            var pX = randVal(extra*MIN_COORD, extra*MAX_COORD);
+            var pY = randVal(extra*MIN_COORD, extra*MAX_COORD);
+            var pZ = randVal(extra*MIN_COORD, extra*MAX_COORD);
+            var particle = new THREE.Vector3(pX, pY, pZ);
+
+            particles.vertices.push(particle);
+        }
+
+        var particleSystem = new THREE.ParticleSystem(particles, pMaterial);
+        scene.add(particleSystem);
+    })();
+   
+    (function makePlanets() {
+        for (var i=0; i<planetArr.length; i++) {
+            var thisPlanet = planetArr[i];
+            
+            var planetMaterial = new THREE.MeshBasicMaterial({
+                color: 0x00ddff,
+                wireframe: true 
+            });
+            var planetGeometry = new THREE.BoxGeometry(100, 100, 100);
+            planetGeometry.applyMatrix(
+                new THREE.Matrix4().makeTranslation(thisPlanet.x, thisPlanet.y, thisPlanet.z)
+            );
+            var planetMesh = new THREE.Mesh(planetGeometry, planetMaterial);
+
+            scene.add(planetMesh);
+        }
+    })();
+
+
 
     renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
+
+
+    (function setUpCamera() {
+        controls = new THREE.TrackballControls( camera );
+
+        controls.rotateSpeed = 1.0;
+        controls.zoomSpeed = 1.2;
+        controls.panSpeed = 0.8;
+
+        controls.noZoom = false;
+        controls.noPan = false;
+
+        controls.staticMoving = true;
+        controls.dynamicDampingFactor = 0.3;
+
+        controls.keys = [ 65, 83, 68 ];
+
+        controls.addEventListener( 'change', renderer.render );
+    })();
 
     document.body.appendChild(renderer.domElement);
 }
@@ -84,13 +109,16 @@ function animate() {
     mesh.rotation.y += 0.02;
 
     // Rotate camera
-    camera.position.x = Math.cos(time*TIME_COEFF) * 1000;
-    camera.position.z = Math.sin(time*TIME_COEFF) * 1000;
-    camera.position.y = Math.cos(time*TIME_COEFF) * 1000;
-    camera.lookAt( new THREE.Vector3(0,0,0) );
+    function rotateCam() {
+        camera.position.x = Math.cos(time*TIME_COEFF) * 1000;
+        camera.position.z = Math.sin(time*TIME_COEFF) * 1000;
+        camera.position.y = Math.cos(time*TIME_COEFF) * 1000;
+        camera.lookAt( new THREE.Vector3(0,0,0) );
+    }
+    //rotateCam();
 
     renderer.render(scene, camera);
-
+    controls.update();
 }
 
 
