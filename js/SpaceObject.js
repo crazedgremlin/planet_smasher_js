f3c622be9f8e483ba7f429fe7fb172780d558f3a
Dan McArdle <zingermc@gmail.com>
2014-06-18 21:16:43 -0400

    added basic planet code

---
+++ js/SpaceObject.js
@@ -0,0 +1,11 @@
+SpaceObject = function() {
+    // positions
+    this.x = 0;
+    this.y = 0;
+    this.z = 0;
+
+    // velocities
+    this.v_x = 0;
+    this.v_y = 0;
+    this.v_z = 0;
+}
--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
+var MIN_COORD = -1000;
+var MAX_COORD = 1000;
 var TIME_COEFF = 0.005;
 
 var camera, scene, renderer;
 var geometry, material, mesh;
+
 var time = 0;
+var planetArr = createPlanets();
+var projectile = new SpaceObject();
+
 
 init();
 animate();
@@ -14,7 +20,7 @@ function init() {
 
     scene = new THREE.Scene();
 
-    geometry = new THREE.BoxGeometry(200, 200, 200);
+    geometry = new THREE.BoxGeometry(50,50,50);
     material = new THREE.MeshBasicMaterial({
         color: 0xff0000,
         wireframe: true 
@@ -33,13 +39,10 @@ function init() {
    
     for (var p=0; p<particleCount; p++) {
 
-        function randVal(min, max) {
-            return Math.random() * (max-min) + min;
-        }
-
-        var pX = randVal(-500, 500);
-        var pY = randVal(-500, 500);
-        var pZ = randVal(-500, 500);
+        var extra = 1.5;
+        var pX = randVal(extra*MIN_COORD, extra*MAX_COORD);
+        var pY = randVal(extra*MIN_COORD, extra*MAX_COORD);
+        var pZ = randVal(extra*MIN_COORD, extra*MAX_COORD);
         var particle = new THREE.Vector3(pX, pY, pZ);
 
         particles.vertices.push(particle);
@@ -48,6 +51,21 @@ function init() {
     var particleSystem = new THREE.ParticleSystem(particles, pMaterial);
     scene.add(particleSystem);
 
+    for (var i=0; i<planetArr.length; i++) {
+        var thisPlanet = planetArr[i];
+        
+        var planetMaterial = new THREE.MeshBasicMaterial({
+            color: 0x00ddff,
+            wireframe: true 
+        });
+        var planetGeometry = new THREE.BoxGeometry(100, 100, 100);
+        planetGeometry.applyMatrix(
+            new THREE.Matrix4().makeTranslation(thisPlanet.x, thisPlanet.y, thisPlanet.z)
+        );
+        var planetMesh = new THREE.Mesh(planetGeometry, planetMaterial);
+
+        scene.add(planetMesh);
+    }
 
     renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
@@ -61,13 +79,41 @@ function animate() {
     // note: three.js includes requestAnimationFrame shim
     requestAnimationFrame(animate);
 
-    //mesh.rotation.x += 0.01;
-    //mesh.rotation.y += 0.02;
+    // Rotate projectile
+    mesh.rotation.x += 0.01;
+    mesh.rotation.y += 0.02;
 
+    // Rotate camera
     camera.position.x = Math.cos(time*TIME_COEFF) * 1000;
     camera.position.z = Math.sin(time*TIME_COEFF) * 1000;
+    camera.position.y = Math.cos(time*TIME_COEFF) * 1000;
     camera.lookAt( new THREE.Vector3(0,0,0) );
 
     renderer.render(scene, camera);
 
+}
+
+
+function randVal(min, max) {
+    return Math.random() * (max-min) + min;
+}
+
+function createPlanets() {
+    var planets = []; // pirate code?
+    var numPlanets = Math.floor(randVal(3, 7));
+
+    console.log(MIN_COORD + "; " + MAX_COORD);
+    for (var i=0; i<numPlanets; i++) {
+        var thisSpaceObj = new SpaceObject();
+
+        // X and Z are evenly dispersed
+        thisSpaceObj.x = randVal(MIN_COORD, MAX_COORD);
+        thisSpaceObj.z = randVal(MIN_COORD, MAX_COORD);
+        // Y is artifically limited to a small range, keeping planets generally on a plane
+        thisSpaceObj.y = randVal(MIN_COORD, MAX_COORD)/20;
+
+        planets[i] = thisSpaceObj;
+    }
+
+    return planets;
 }
